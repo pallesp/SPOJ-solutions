9b1bc0a56f4c64082f697014704f100e4c37fbf5
Sai krishna <pallesai1995@gmail.com>
2015-07-21 11:18:34 +0530

    Create AMZSEQ.cpp

---
+++ AMZSEQ.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[])
+	{
+		long long int a[24];
+		int t, x;
+ 
+		a[0] = 1;
+		a[1] = 3;
+ 
+		for(int i = 2; i < 24; i++)
+			a[i] = 2*a[i-1] + a[i-2];
+	
+		cin >> x;
+		cout << a[x] << endl;			
+		
+		return 0;
+	} 
