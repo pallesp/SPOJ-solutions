c7586c832462583ba377f383bef9455b177b0df3
Sai krishna <pallesai1995@gmail.com>
2015-07-21 11:39:05 +0530

    Create BOMARBLE.cpp

---
+++ BOMARBLE.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+#include<algorithm>
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<vector>
+#include<list>
+#define ll long long
+#define ri(T) scanf("%d",&T)
+#define rl(T) scanf("%lld",&T)
+#define MOD 1000000007
+using namespace std;
+#include<stdio.h>
+int main()
+{
+	ll t,n,p,res;
+	rl(t);
+	while(t)
+	{
+		cout<<(3*t*t+5*t+2)/2<<endl;
+		rl(t);
+	}
+}
