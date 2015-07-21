3f02311405bfded3d7ce1900e192a581bebbc70e
Sai krishna <pallesai1995@gmail.com>
2015-07-21 12:44:02 +0530

    Create OLOLO.cpp

---
+++ OLOLO.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    long long int arr[500001];
+    for(i=0;i<n;i++)
+        scanf("%lld",&arr[i]);
+    long long int a,ans;
+    a=arr[0];
+    for(i=1;i<n;i++)
+    {
+        ans=a^arr[i];
+        a=ans;
+    }
+    printf("%lld\n",ans);
+    return 0;
+}
