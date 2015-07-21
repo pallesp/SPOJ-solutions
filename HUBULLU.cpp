398b7706fc29f775225e56b2fee2595befd83a66
Sai krishna <pallesai1995@gmail.com>
2015-07-21 12:21:33 +0530

    Create HUBULLU.cpp

---
+++ HUBULLU.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;	
+	cin>>t;
+	while(t--)
+	{
+		long long a,b;
+		cin>>a>>b;
+		if(b==0)
+		{
+			cout<<"Airborne wins.\n";
+		}	
+		else
+		{
+			cout<<"Pagfloyd wins.\n";
+		}
+	}
+}
